74a752519566685e50a2ae93eb2356fd46bfa51d
Caitlin Pisha <caitlinpisha@Caitlins-MacBook-Pro.local>
2016-06-10 09:49:09 -0500

    Added images, removed old theme files, and hard coded Raleway into the theme

--- Exercise 11 - WordPress Site/wordpress/wp-content/themes/spacious/header.php
+++ Exercise 11 - WordPress Site/wordpress/wp-content/themes/spacious/header.php
@@ -24,6 +24,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link href='https://fonts.googleapis.com/css?family=Raleway:400,900,700' rel='stylesheet' type='text/css'>
 <?php
 /**
  * This hook is important for wordpress plugins and other many things
